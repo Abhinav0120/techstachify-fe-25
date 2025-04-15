d84de2a450aea95a9562aeac5fd305f318ce37e4
abhinav <ajagekarajagekar@gmail.com>
2025-04-15 20:09:44 +0530

    chore(config): initial project setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'revert', 'added', 'fixed', 'removed'],
+		],
+		'scope-enum': [2, 'always', ['frontend', 'backend', 'config', 'docs', 'test', 'build']],
+		'scope-case': [2, 'always', ['lower-case', 'camel-case']],
+		'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case', 'sentence-case']],
+		'subject-full-stop': [2, 'never'],
+		'header-max-length': [2, 'always', 100],
+	},
+};
